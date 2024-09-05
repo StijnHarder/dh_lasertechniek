d8614cb92854cc322e8ae40892c71116d77cc598
Stijn Harder <stijn.harder@hva.nlm>
2024-09-05 09:45:46 +0200

    POST request instead of GET for fetching

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -45,37 +45,22 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { Suspense } from "react";
-import Loading from "./Loading";
-
-type PostProps = {
-  title: string;
-  excerpt: string;
-  slug: string;
-  featuredImage: {
-    node: {
-      sourceUrl: string;
-      altText: string;
-    };
-  };
-  content: string;
-};
 
 async function getPosts() {
   const query = `
-  {
-    posts(first: 5) {
+    {
+    posts {
       nodes {
-        excerpt
         featuredImage {
           node {
             sourceUrl
             altText
           }
         }
-        slug
-        title
         content
+        slug
+        excerpt
+        title
       }
     }
   }`;
@@ -85,11 +70,12 @@ async function getPosts() {
       process.env.NEXT_PUBLIC_WORDPRESS_API_ENDPOINT
     }?query=${encodeURIComponent(query)}`,
     {
-      method: "GET",
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      next: { revalidate: 60 },
+      body: JSON.stringify({ query }),
+      next: { revalidate: 10 },
     }
   );
 
@@ -106,21 +92,19 @@ export default async function PostList() {
       <div className="flex flex-col p-6 gap-6 bg-[#FFFFFF] border-[1px] border-[#E8E8E8] rounded-lg">
         <h1 className="text-4xl font-bold">D&H Lasertechniek Blog</h1>
         <div className="flex gap-4">
-          {posts.map((post: PostProps) => (
+          {posts.map((post: any) => (
             <div key={post.slug}>
-              <Suspense fallback={<Loading />}>
-                <Link href={`/blog/${post.slug}`}>
-                  <div className="relative h-80 transition-all duration-200 ease-linear hover:-translate-y-[3px]">
-                    <Image
-                      src="/placeholder_background.jpg"
-                      alt={post.title}
-                      className="absolute rounded-md h-full w-full object-cover"
-                      width={500}
-                      height={500}
-                    />
-                  </div>
-                </Link>
-              </Suspense>
+              <Link href={`/blog/${post.slug}`}>
+                <div className="relative h-80 transition-all duration-200 ease-linear hover:-translate-y-[3px]">
+                  <Image
+                    src="/placeholder_background.jpg"
+                    alt={post.title}
+                    className="absolute rounded-md h-full w-full object-cover"
+                    width={500}
+                    height={500}
+                  />
+                </div>
+              </Link>
               <Link href={`/blog/${post.slug}`} className="post-content my-4">
                 <h3 className="text-2xl pt-4 pb-2">{post.title}</h3>
                 <p dangerouslySetInnerHTML={{ __html: post.excerpt }}></p>
