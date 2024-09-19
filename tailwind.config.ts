d3842b31df6d24e54d3f2c63d05881d2c1065009
Stijn Harder <stijn.harder@hva.nlm>
2024-09-19 15:23:30 +0200

    delete redundant colors tailwind css config.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,21 +11,11 @@ const config: Config = {
       // Define your color palette
       colors: {
         primary: {
-          light: '#5B84D7', // still to set
-          DEFAULT: '#1E40AF', // still to set
           dark: '#454A63', // primary text blue
-        },
-        secondary: {
-          light: '#F6C95F', // still to set
-          DEFAULT: '#F59E0B', // still to set
-          dark: '#D97706', // still to set
         },
         neutral: {
           light: '#FBFBFC', // primary bg light grey with blue
-          DEFAULT: '#D1D5DB', // still to set
-          dark: '#4B5563', // still to set
         },
-        accent: '#EC4899', // still to set
         background: '#FBFBFC',
         border_gray: '#E8E8E8',
         button_yellow: '#FFD157',
